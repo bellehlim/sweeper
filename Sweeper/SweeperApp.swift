2a4d43579b03f6e3f11b873f7c4adcd75a895dc5
bellehlim@gmail.com <bellehlim@gmail.com>
2024-07-23 16:47:06 -0400

    Initial Commit

---
+++ Sweeper/SweeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  SweeperApp.swift
+//  Sweeper
+//
+//  Created by Belle Lim on 7/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SweeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
